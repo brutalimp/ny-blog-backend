0bbefcb06bd1b11ad1ec01d753b16ffb6d725461
Peter Zhang <913542647@qq.com>
2018-05-27 01:06:42 +0800

    fix script issue

--- hooks/post-receive.sh
+++ hooks/post-receive.sh
@@ -4,8 +4,14 @@ GIT_DIR="/home/gitrepo/ny-blog-backend.git"
 APP_NAME="ny-blog-backend"
 BRANCH="master"
 
+
 echo "post-receive: Triggered."
 cd $TARGET
+
+if [ ! -d "$TARGET" ]; then
+  echo "mkdir $TARGET"
+  mkdir $TARGET
+fi
 
 echo "post-receive: git check out..."
 git --git-dir=$GIT_DIR  --work-tree=$TARGET checkout $BRANCH -f
